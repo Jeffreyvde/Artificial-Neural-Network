1ccb5f237beddeb2ad2a2a38a3b636c59aed348b
Jeffrey van den Elshout <jeffreyvde15@gmail.com>
2019-09-15 16:28:20 +0200

    Improve neuron class

--- Artififical Neural Network/Neural Network/Basics/Neuron.cs
+++ Artififical Neural Network/Neural Network/Basics/Neuron.cs
@@ -1,18 +1,34 @@
-﻿namespace NeuralNetwork
+﻿using System;
+
+namespace NeuralNetwork
 {
     public class Neuron
     {
-        public int layerIndex;
+        public readonly int layerIndex;
+        public readonly int layerRow;
 
         public float activation;
 
         public float bias;
         public float weightedSum;
 
-         /// <summary>
-         /// Cal;culate the activation of this neuron
-         /// </summary>
-         /// <param name="sum"></param>
+        /// <summary>
+        /// Constructor for Neuron class. That generates random bias between 0 and 1.
+        /// </summary>
+        /// <param name="layerIndex"></param>
+        /// <param name="random"></param>
+        public Neuron(int layerIndex, int layerRow, Random random)
+        {
+            this.layerIndex = layerIndex;
+            this.layerRow = layerRow;
+
+            bias = (float)random.NextDouble();
+        }
+
+        /// <summary>
+        /// Cal;culate the activation of this neuron
+        /// </summary>
+        /// <param name="sum"></param>
         public void CalculateActivation(float sum)
         {
 
