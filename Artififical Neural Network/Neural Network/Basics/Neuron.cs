f2a0e9da9ed112a0e1256f1d8ff165e7eb583a56
Jeffrey van den Elshout <jeffreyvde15@gmail.com>
2019-09-15 15:58:38 +0200

    Add neuron class

---
+++ Artififical Neural Network/Neural Network/Basics/Neuron.cs
@@ -0,0 +1,21 @@
+﻿namespace NeuralNetwork
+{
+    public class Neuron
+    {
+        public int layerIndex;
+
+        public float activation;
+
+        public float bias;
+        public float weightedSum;
+
+         /// <summary>
+         /// Cal;culate the activation of this neuron
+         /// </summary>
+         /// <param name="sum"></param>
+        public void CalculateActivation(float sum)
+        {
+
+        }
+    }
+}
