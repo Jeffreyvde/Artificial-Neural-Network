40e517d261bc3c2ab3b1356d97949eedcccca450
Jeffrey van den Elshout <jeffreyvde15@gmail.com>
2019-09-15 15:42:31 +0200

    Add basic c# project

---
+++ Artififical Neural Network/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artififical_Neural_Network
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
