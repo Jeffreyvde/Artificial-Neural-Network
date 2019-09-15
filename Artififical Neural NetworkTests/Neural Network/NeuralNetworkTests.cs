fac52dc72955ab1cf1c96c1e5b8478ac81d02e99
Jeffrey van den Elshout <jeffreyvde15@gmail.com>
2019-09-15 20:14:51 +0200

    Add unit tests

--- Artififical Neural NetworkTests/Neural Network/NeuralNetworkTests.cs
+++ Artififical Neural NetworkTests/Neural Network/NeuralNetworkTests.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeuralNetwork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Artififical_Neural_NetworkTests.Neural_Network
+namespace NeuralNetwork.Tests
 {
-    class NeuralNetworkTests
+    [TestClass()]
+    public class NeuralNetworkTests
     {
+        [TestMethod()]
+        public void LayerWeightsInitialized()
+        {
+            NeuralNetwork neuralNetwork = new NeuralNetwork(3, 3, 3);
+
+            for (int i = 1; i < neuralNetwork.layers.Length; i++)
+            {
+                
+                Assert.IsNotNull(neuralNetwork.layers[i].weights, "Weights of network layers not initialized");
+
+            }
+        }
+
+        [TestMethod()]
+        public void LayerNeuronsInitialized()
+        {
+            NeuralNetwork neuralNetwork = new NeuralNetwork(3, 3, 3);
+
+            for (int i = 0; i < neuralNetwork.layers.Length; i++)
+            {
+                Assert.IsNotNull(neuralNetwork.layers[i].neurons, "Neurons of network layes not initialized");
+            }
+        }
     }
 }
