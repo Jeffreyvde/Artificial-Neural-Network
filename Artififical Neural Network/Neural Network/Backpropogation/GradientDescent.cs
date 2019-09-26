0fc50f3f1e3d914a3b3065c17f406c3ad1661b24
Jeffreyvde <jeffrey.vd.elshout@gmail.com>
2019-09-26 10:06:48 +0200

    Add Arithmetic Operators

--- Artififical Neural Network/Neural Network/Backpropogation/GradientDescent.cs
+++ Artififical Neural Network/Neural Network/Backpropogation/GradientDescent.cs
@@ -24,5 +24,44 @@ namespace NeuralNetworks
                 steps[i].Apply();
             }
         }
+
+        #region ArithmeticOperators
+
+
+        /// <summary>
+        /// + operator for gradient descents
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static GradientDescent operator +(GradientDescent a, GradientDescent b)
+        {
+            GradientDescent newDescent = new GradientDescent();
+            for (int i = 0; i < a.steps.Count; i++)
+            {
+                newDescent.Add(a.steps[i].stepValue + b.steps[i].stepValue, a.steps[i].changedObject);
+            }
+
+            return newDescent;
+        }
+
+        /// <summary>
+        /// Gradient descent 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="division"></param>
+        /// <returns></returns>
+        public static GradientDescent operator /(GradientDescent a, int division)
+        {
+            for (int i = 0; i < a.steps.Count; i++)
+            {
+                GradientDecentValue step = a.steps[i];
+                step.stepValue /= division;
+            }
+
+            return a;
+        }
+        #endregion
+
     }
 }
