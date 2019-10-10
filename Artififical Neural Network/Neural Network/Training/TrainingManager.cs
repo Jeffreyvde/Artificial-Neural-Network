8ebe02f71de644ff6309b5bcbe07f2d364c142f2
Jeffreyvde <jeffrey.vd.elshout@gmail.com>
2019-10-10 09:18:02 +0200

    Add generate batches and train until

--- Artififical Neural Network/Neural Network/Training/TrainingManager.cs
+++ Artififical Neural Network/Neural Network/Training/TrainingManager.cs
@@ -8,8 +8,9 @@ namespace NeuralNetworks
 {
     public class TrainingManager
     {
-        private readonly Batch[,] batches;
-        private readonly int iterations, batchesPerIterations, iterationSize;
+        private Batch[,] batches;
+        private readonly TrainingData[] data;
+        private readonly int iterations, batchesPerIterations, batchSize, iterationSize;
 
 
         private TrainingData[] testData;
@@ -26,12 +27,26 @@ namespace NeuralNetworks
         public TrainingManager(int iterations, TrainingData[] data, TrainingData[] testData, int batchesPerIterations, int batchSize, NeuralNetwork neuralNetwork, double learningRate = 3)
         {
             batches = new Batch[iterations, batchesPerIterations];
+
+            this.data = data;
             this.batchesPerIterations = batchesPerIterations;
             this.iterations = iterations;
             this.learningRate = learningRate;
             this.testData = testData;
+            this.batchSize = batchSize;
+
             iterationSize = batchesPerIterations * data[0].inputData.Length;
 
+            GenerateBatches();
+            this.neuralNetwork = neuralNetwork;
+        }
+
+        
+        /// <summary>
+        /// Generate new batches 
+        /// </summary>
+        private void GenerateBatches()
+        {
             for (int i = 0; i < iterations; i++)
             {
                 for (int j = 0; j < batchesPerIterations; j++)
@@ -39,42 +54,54 @@ namespace NeuralNetworks
                     batches[i, j] = new Batch(data, batchSize);
                 }
             }
-            this.neuralNetwork = neuralNetwork;
+        }
+
+        /// <summary>
+        /// Train until a specific required percentage
+        /// </summary>
+        /// <param name="requiredPercentage"></param>
+        public void TrainUntil(float requiredPercentage, string savepath = "")
+        {
+            do
+            {
+                GenerateBatches();
+                Train();
+                if (savepath != "")
+                    neuralNetwork.Save(savepath);
+
+            } while (Test() < requiredPercentage);
         }
 
         /// <summary>
         /// Train the neural network via batches
         /// </summary>
         /// <param name="network"></param>
-        public void Train(string path)
+        public void Train()
         {
-            while (true)
-            {
-                for (int i = 0; i < iterations; i++)
-                {
-                    GradientDescent gradientDescent = batches[0,0].Run(neuralNetwork);
-                    List<Task> TaskList = new List<Task>();
-                    for (int j = i; j < batchesPerIterations; j++)
-                    {
-                        if (i == 0 && j == 0) continue;
 
-                        Batch batch = batches[i, j];
-                        Task task = Task.Run(() => gradientDescent += batch.Run(neuralNetwork));
-                        TaskList.Add(task);
-                    }
-                    Task.WaitAll(TaskList.ToArray());
-                    gradientDescent.Apply(learningRate / iterationSize);
-                    Console.WriteLine("Finished iteration: " + i);
+            for (int i = 0; i < iterations; i++)
+            {
+                GradientDescent gradientDescent = batches[0, 0].Run(neuralNetwork);
+                List<Task> TaskList = new List<Task>();
+                for (int j = i; j < batchesPerIterations; j++)
+                {
+                    if (i == 0 && j == 0) continue;
+
+                    Batch batch = batches[i, j];
+                    Task task = Task.Run(() => gradientDescent += batch.Run(neuralNetwork));
+                    TaskList.Add(task);
                 }
-                Test();
-                neuralNetwork.Save(path);
+                Task.WaitAll(TaskList.ToArray());
+                gradientDescent.Apply(learningRate / iterationSize);
+                Console.WriteLine("Finished iteration: " + i);
             }
         }
+
 
         /// <summary>
         /// Test the neural network
         /// </summary>
-        private void Test()
+        private float Test()
         {
             float correctThings = 0;
             for (int i = 0; i < testData.Length; i++)
@@ -88,6 +115,7 @@ namespace NeuralNetworks
             }
             float percentage = correctThings / testData.Length * 100f;
             Console.WriteLine("Network Percentage: " + percentage);
+            return percentage;
         }
     }
 }
