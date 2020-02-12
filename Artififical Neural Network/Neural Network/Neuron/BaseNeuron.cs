a7c68bbe960cb90d2ca6667cd7e6bdd0b7b88a95
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2020-02-12 20:08:09 +0100

    Add base neuron

---
+++ Artififical Neural Network/Neural Network/Neuron/BaseNeuron.cs
@@ -0,0 +1,19 @@
+﻿namespace NeuralNetworks.Neurons
+{
+    /// <summary>
+    /// THis is the class designed for a basic Neuron. It only holds an activation value.
+    /// </summary>
+    public class BaseNeuron
+    {
+        public double Acitvation { get; private set; }
+
+        /// <summary>
+        /// Constructor for the base neuron
+        /// </summary>
+        /// <param name="activation"></param>
+        public BaseNeuron(double activation)
+        {
+            Acitvation = activation;
+        }
+    }
+}
