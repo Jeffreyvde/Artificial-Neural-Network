ef697f635dcafd7507925e4017ff007ad8e6ba68
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2020-02-14 22:37:32 +0100

    Add new layer class

---
+++ Artififical Neural Network/Neural Network/Layers/Layer.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using NeuralNetwork.Neurons;
+
+namespace NeuralNetwork.Layers
+{
+    public abstract class Layer<T> where T : BaseNeuron
+    {
+        public T[] Neurons { get; protected set; }
+
+        /// <summary>
+        /// Default constructor for layers
+        /// </summary>
+        protected Layer() { }
+
+        #region Initialization
+        [JsonConstructor()]
+        public Layer(T[] neurons)
+        {
+            Neurons = neurons;
+        }
+
+        /// <summary>
+        /// Generate weights for this layer
+        /// </summary>
+        /// <param name="previousNeurons">previous neurons to create Connection</param>
+        public void ConnectNeurons(BaseNeuron[] nextLayer, BaseNeuron[] previousLayer)
+        {
+            for (int i = 0; i < Neurons.Length; i++)
+            {
+                Neurons[i].EstablishConnections(nextLayer, previousLayer);
+            }
+        }
+        #endregion
+        #region Training
+
+        /// <summary>
+        /// Train this layer
+        /// </summary>
+        /// <param name="previousNeurons"></param>
+        public void FeedForward()
+        {
+            for (int i = 0; i < Neurons.Length; i++)
+            {
+                Neurons[i].FeedForward();
+            }
+        }
+
+        #endregion
+        #region Backpropogation
+
+        /// <summary>
+        /// Backpropogate this output layer
+        /// </summary>
+        /// <param name="correctOutputNeuron"></param>
+        public void BackPropogate(GradientDescent gradient)
+        {
+            for (int i = 0; i < Neurons.Length; i++)
+            {
+                Neurons[i].BackPropogate(gradient);
+            }
+        }
+        #endregion
+    }
+}
