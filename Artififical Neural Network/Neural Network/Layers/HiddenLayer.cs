0bc5bb5316252fca3b533048f29b8c42fbe9b83c
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2020-02-14 22:37:47 +0100

    Add hidden layer child class

---
+++ Artififical Neural Network/Neural Network/Layers/HiddenLayer.cs
@@ -0,0 +1,25 @@
+﻿using NeuralNetwork.Neurons;
+
+namespace NeuralNetwork.Layers
+{
+    /// <summary>
+    /// Dense layer the basic building block for a Neural Network
+    /// </summary>
+    public class DenseLayer : Layer<HiddenNeuron>
+    {
+        /// <summary>
+        /// Initialises the Layer with Hidden neurons
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="activatFunction"></param>
+        public DenseLayer(int size, IActivation activatFunction)
+        {
+            Neurons = new HiddenNeuron[size];
+            for (int i = 0; i < Neurons.Length; i++)
+            {
+                Neurons[i] = new HiddenNeuron(activatFunction);
+            }
+        }
+
+    }
+}
