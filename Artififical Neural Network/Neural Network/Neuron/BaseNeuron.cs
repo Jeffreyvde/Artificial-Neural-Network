291a38bedbf204cdb101ecac25d9924927147203
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2020-02-14 22:08:11 +0100

    Implement new required functions to base neuron

--- Artififical Neural Network/Neural Network/Neuron/BaseNeuron.cs
+++ Artififical Neural Network/Neural Network/Neuron/BaseNeuron.cs
@@ -1,11 +1,15 @@
-﻿namespace NeuralNetworks.Neurons
+﻿using Newtonsoft.Json;
+
+namespace NeuralNetwork.Neurons
 {
     /// <summary>
-    /// THis is the class designed for a basic Neuron. It only holds an activation value.
+    /// This is the class designed for a basic Neuron. It only holds an activation value.
     /// </summary>
-    public class BaseNeuron
+    public abstract class BaseNeuron
     {
-        public double Acitvation { get; private set; }
+        [JsonIgnore] public double Activation { get; protected set; }
+        public Connection[] ForwardConnections { get; protected set; }
+        public Connection[] BackwardsConnections { get; protected set; }
 
         /// <summary>
         /// Constructor for the base neuron
@@ -13,7 +17,53 @@
         /// <param name="activation"></param>
         public BaseNeuron(double activation)
         {
-            Acitvation = activation;
+            Activation = activation;
+        }
+
+
+        /// <summary>
+        /// Empty constructor only for child classes (Activation for certain neurons will be set later)
+        /// </summary>
+        protected BaseNeuron() { }
+
+        /// <summary>
+        /// Constructor used for json parsing
+        /// </summary>
+        /// <param name="activation"></param>
+        /// <param name="forwardConnections"></param>
+        /// <param name="backwardsConnections"></param>
+        [JsonConstructor]
+        public BaseNeuron(double activation, Connection[] forwardConnections, Connection[] backwardsConnections) : this(activation)
+        {
+            ForwardConnections = forwardConnections;
+            BackwardsConnections = backwardsConnections;
+        }
+
+        /// <summary>
+        /// Set the connection for this neuron
+        /// </summary>
+        /// <param name="forward"></param>
+        /// <param name="backwards"></param>
+        public void SetConnections(Connection[] forward, Connection[] backwards)
+        {
+            ForwardConnections = forward;
+            BackwardsConnections = backwards;
+        }
+
+        /// <summary>
+        /// Feedforward the Neuron
+        /// </summary>
+        public abstract void FeedForward();
+
+        /// <summary>
+        /// Backpropogate the Neuron
+        /// </summary>
+        public virtual void BackPropogate(GradientDescent descent)
+        {
+            for (int i = 0; i < ForwardConnections.Length; i++)
+            {
+                ForwardConnections[i].BackPropogate();
+            }
         }
     }
 }
