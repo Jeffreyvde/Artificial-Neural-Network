f3d27e8f476690c1300c7afa9a24e8ed5950c53e
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2020-02-18 10:38:54 +0100

    Improve GradientDescentValue

--- Artififical Neural Network/Neural Network/Backpropogation/GradientDecentValue.cs
+++ Artififical Neural Network/Neural Network/Backpropogation/GradientDecentValue.cs
@@ -2,16 +2,25 @@
 {
     public class GradientDecentValue
     {
-        public double stepValue;
+        private double stepValue;
 
-        public IBackpropogatable changedObject;
+        private IBackpropogatable changedObject;
 
+        /// <summary>
+        /// Create gradient descent value
+        /// </summary>
+        /// <param name="stepValue"></param>
+        /// <param name="changedObject"></param>
         public GradientDecentValue(double stepValue, IBackpropogatable changedObject)
         {
             this.stepValue = stepValue;
             this.changedObject = changedObject;
         }
 
+        /// <summary>
+        /// Apply to the changed object
+        /// </summary>
+        /// <param name="learningRate"></param>
         public void Apply(double learningRate)
         {
             changedObject.ApplyGradientDecentStep(stepValue, learningRate);
